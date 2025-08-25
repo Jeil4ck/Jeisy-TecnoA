0c4a4df0a111fcc6877515ac88bbff8232a12137
Jeisy Bermudez <jlbermudez@unillanos.edu.co>
2025-08-24 21:44:56 -0500

    Actualizar Main: <breve detalle>

--- src/main/java/com/myapp/Main.java
+++ src/main/java/com/myapp/Main.java
@@ -13,9 +13,9 @@ public class Main {
         Programa progIng = new Programa(1, "Ing. Sistemas", 10.0, java.time.LocalDate.now(), null);
         Curso avanzadas = new Curso(101.0, "Tecnologias Avanzadas", true);
 
-        Profesor prof1 = new Profesor(2001, "Roger", "Herrera", "RogerH@unillanos.edu.co", "Planta");
+        Profesor prof1 = new Profesor(2001, "Roger", "Calderon", "RogerC@unillanos.edu.co", "Planta");
         Estudiante est1 = new Estudiante(1001, "Jeisy", "Bermudez", "jlbermudez@unillanos.edu.co", 4802, progIng, true, 4.7);
-        Estudiante est2 = new Estudiante(1002, "Andrea", "rojas", "ARojas@unillanos.edu.co", 5002, progIng, true, 4.8);
+        Estudiante est2 = new Estudiante(1002, "Andrea", "Rojas", "ARojas@unillanos.edu.co", 5002, progIng, true, 4.8);
 
         // Lista en memoria (diagrama)
         repo.inscribir(prof1);
